9fe072eadd456db9f76584bbde184f4c17482b35
aakash-1702 <dwivediakash1702@gmail.com>
2026-09-11 22:25:16 +0530

    Time: 62 ms (16.13%) | Memory: 94.7 MB (20.24%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    bool repeatedSubstringPattern(string s) {
-        string a = "";
-        for(auto &x : s){
-            a += x;
-            if(a.size() == s.size()) return false;
-            if((s.size() % a.size()) == 0  && isPossible(a , s)) return true;
-        }        
-
-        return false;
-    }
-    private:
-    bool isPossible(string &a , string &b){
-        int t = b.size() / a.size();
-        string c = "";
-        for(int i = 0 ;i < t ; i++){
-            c += a;
+     bool repeatedSubstringPattern(string s) {
+        int n = s.size();
+        
+        // Iterate over all possible lengths of the substring (up to half of the string length)
+        for (int len = 1; len <= n / 2; ++len) {
+            // If the current length can divide the string evenly
+            if (n % len == 0) {
+                string sub = s.substr(0, len);
+                string repeated = "";
+                
+                // Rebuild the string by repeating the substring
+                for (int i = 0; i < n / len; ++i) {
+                    repeated += sub;
+                }
+                
+                // Check if the rebuilt string matches the original
+                if (repeated == s) {
+                    return true;
+                }
+            }
         }
-
-        return b == c;
+        return false;
     }
 };
